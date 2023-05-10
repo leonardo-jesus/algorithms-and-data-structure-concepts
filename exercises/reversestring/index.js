f4b218a41a4a13cab31005d6cebdc1e645312880
Leonardo Jesus <leonardojesus02@outlook.com>
2023-05-10 00:59:33 -0300

    feat(reversestring): add reverse string solution

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,27 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
-
-module.exports = reverse;
+function reverse(str) {
+    let reversedString = '';
+  
+    for (let i = 1; i <= str.length; i++) {
+      reversedString = `${reversedString}${str[str.length - i]}`;
+    };
+  
+    return reversedString;
+  }
+  
+  reverse('asdf');
+  
+  module.exports = reverse;
+  
+  // function reverse(str) {
+  //   let reversedString = '';
+  
+  //   for (let i = 0; i < str.length; i++) {
+  //     reversedString = `${str[i]}${reversedString}`;
+  //   };
+  
+  //   return reversedString;
+  // }
+  
